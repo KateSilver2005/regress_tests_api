aa792166cb6c4beca9e82285daa4f96a05a6a505
Alexeeva_Kate <114491314+KateSilver2005@users.noreply.github.com>
2025-05-18 18:43:24 +0300

    Add files via upload

---
+++ Data/__init__.py
@@ -0,0 +1,5 @@
+from .path_and_time import *
+from .logger import *
+from .endpoints import endpoints
+from .Data import Search, Raiting, Graphs, Licenses, Ki_Lp_ki_mi, Profile, Snippets, Cards, Sciwork, Filters
+from .headers import *
---
+++ Data/headers.py
@@ -0,0 +1,78 @@
+from datetime import datetime
+
+headers_post = {
+    'server': 'nginx/1.26.2',
+    'date': '',
+    'content-type': 'application/json',
+    'content-length': '',
+    'vary': 'Accept, Origin',
+    'allow': 'POST',
+    'x-frame-options': 'DENY',
+    'x-content-type-options': 'nosniff',
+    'referrer-policy': 'same-origin',
+    'cross-origin-opener-policy': 'same-origin'}
+
+
+headers_post_options = {
+    'server': 'nginx/1.26.2',
+    'date': '',
+    'content-type': 'application/json',
+    'content-length': '',
+    'vary': 'Accept, Origin',
+    'allow': 'POST, OPTIONS',
+    'x-frame-options': 'DENY',
+    'x-content-type-options': 'nosniff',
+    'referrer-policy': 'same-origin',
+    'cross-origin-opener-policy': 'same-origin'}
+
+
+headers_get = {
+    'server': 'nginx/1.26.2',
+    'date': '',
+    'content-type': 'application/json',
+    'content-length': '',
+    'vary': 'Accept, Origin',
+    'allow': 'GET',
+    'x-frame-options': 'DENY',
+    'x-content-type-options': 'nosniff',
+    'referrer-policy': 'same-origin',
+    'cross-origin-opener-policy': 'same-origin'}
+
+
+headers_get_head_options = {
+    'server': 'nginx/1.26.2',
+    'date': '',
+    'content-type': 'application/json',
+    'content-length': '',
+    'vary': 'Accept, Origin',
+    'allow': 'GET, HEAD, OPTIONS',
+    'x-frame-options': 'DENY',
+    'x-content-type-options': 'nosniff',
+    'referrer-policy': 'same-origin',
+    'cross-origin-opener-policy': 'same-origin'}
+
+
+headers_get_post_head_options = {
+    'server': 'nginx/1.26.2',
+    'date': '',
+    'content-type': 'application/json',
+    'content-length': '',
+    'vary': 'Accept, Origin',
+    'allow': 'GET, POST, HEAD, OPTIONS',
+    'x-frame-options': 'DENY',
+    'x-content-type-options': 'nosniff',
+    'referrer-policy': 'same-origin',
+    'cross-origin-opener-policy': 'same-origin'
+}
+
+headers_get_put_delete = {
+    'server': 'nginx/1.26.2',
+    'date': '',
+    'content-type': 'application/json',
+    'content-length': '',
+    'vary': 'Accept, Origin',
+    'allow': 'GET, PUT, DELETE',
+    'x-frame-options': 'DENY',
+    'x-content-type-options': 'nosniff',
+    'referrer-policy': 'same-origin',
+    'cross-origin-opener-policy': 'same-origin'}
