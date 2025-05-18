50e4d7e5352ffdc37ae41cef096d4718f2f18dec
Alexeeva_Kate <114491314+KateSilver2005@users.noreply.github.com>
2025-05-18 18:42:13 +0300

    Add files via upload

---
+++ regress_test_ias_api/tool/__init__.py
@@ -0,0 +1,13 @@
+from .base import (
+    send_message_mt,
+    send_message_file_mt,
+    request_post,
+    request_get
+)
+
+__all__ = (
+    "send_message_mt",
+    "send_message_file_mt",
+    "request_post",
+    "request_get"
+)
